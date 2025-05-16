09cb675339ce55b28c521e5386a3348049591e00
kq62 <kq62@cornell.edu>
2025-05-16 15:43:36 -0400

    Initial commit for PolarPairs May16 release

---
+++ MAY13/build/cmake/CMakeFiles/CMakeScratch/TryCompile-G7rrod/CMakeFiles/cmTC_f8884.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_f8884.dir/src.c.o"
+  "CMakeFiles/cmTC_f8884.dir/src.c.o.d"
+  "cmTC_f8884"
+  "cmTC_f8884.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_f8884.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
